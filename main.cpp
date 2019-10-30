d8cd2562f4a651e447176971ff7dfbd2aa844a4c
BenjaminDiazGarcia <39738647+BenjaminDiazGarcia@users.noreply.github.com>
2019-10-30 15:03:59 -0500

    feat(BenjaminDiazGarcia): código más limpio

--- main.cpp
+++ main.cpp
@@ -4,20 +4,26 @@
 using namespace std;
 
 struct scanner{
+
     scanner(){}
+
     static vector<string> decode_word(string word){
         vector<string> decoded_word;
         decode_aux(word, decoded_word);
         int size_count = 0;
-        for(int i=0; i<decoded_word.size(); ++i){
+
+        for(int i=0; i < decoded_word.size(); ++i){
             size_count += decoded_word[i].size();
-        }
-        if(size_count != word.size()){
+		} 
+
+		if (size_count != word.size()) {
             cout << "There's a typo somewhere in the number passed "<<endl;
         }
+
         return decoded_word;
     }
-   static void decode_aux(string word, vector<string> & decoded_word) {
+
+   	static void decode_aux(string word, vector<string> & decoded_word) {
 
         if(word.size() == 0){
             return;
@@ -29,18 +35,17 @@ struct scanner{
                     decoded_word.push_back("atch");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
+
             case 'd': {
                 if(word.substr(0,4) == "drei") {
                     decoded_word.push_back("drei");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
@@ -53,91 +58,80 @@ struct scanner{
                     decoded_word.push_back("elf");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else{
                     break;
                 }
-                }
+            }
 
-            case 'f' :{
+            case 'f': {
                 if(word.substr(0,4) == "funf") {
                     decoded_word.push_back("funf");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else{
                     break;
                 }
-                }
+            }
 
-            case 'h' :{
-                    if(word.substr(0,7) == "hundert") {
-                        decoded_word.push_back("hundert");
-                        word.erase(0, 7);
-                        decode_aux(word, decoded_word);
-                    }
-                    else{
-                        break;
-                    }
-                }
+            case 'h': {
+                if(word.substr(0,7) == "hundert") {
+                    decoded_word.push_back("hundert");
+                    word.erase(0, 7);
+                    decode_aux(word, decoded_word);
+                } else {
+                    break;
+      	        }
+            }
 
-            case 'n' :{
+            case 'n': {
                 if(word.substr(0,4) == "neun") {
                     decoded_word.push_back("neun");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
-            case 's' :{
+            case 's': {
                 if(word[4] == 's' && word.substr(4,8) != "sieb" && word.substr(4,8) != "sech" && word.substr(4,8) != "ssig") {
                     decoded_word.push_back("sechs");
                     word.erase(0, 5);
                     decode_aux(word, decoded_word);
-                }
-                else if (word[4] != 's' && word.substr(0,4) == "sech") {
+                } else if (word[4] != 's' && word.substr(0,4) == "sech") {
                     decoded_word.push_back("sech");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,4) == "sieb" && word.substr(4,6) != "en"){
+                } else if (word.substr(0,4) == "sieb" && word.substr(4,6) != "en") {
                     decoded_word.push_back("sieb");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,6) == "sieben"){
+                } else if (word.substr(0,6) == "sieben") {
                     decoded_word.push_back("sieben");
                     word.erase(0, 6);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,4) == "ssig"){
+                } else if (word.substr(0,4) == "ssig") {
                     decoded_word.push_back("ssig");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
-            case 't' :{
+            case 't': {
                 if(word.substr(0,7) == "tausend") {
                     decoded_word.push_back("tausend");
                     word.erase(0, 7);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
-            case 'u' :{
+            case 'u': {
                 if(word.substr(0,3) == "und") {
                     decoded_word.push_back("und");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
@@ -146,64 +140,51 @@ struct scanner{
                     decoded_word.push_back("vier");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
 
             case 'z' :{
-                if( word.substr(0,4) =="zwei" ) {
+                if( word.substr(0,4) == "zwei" ) {
                     decoded_word.push_back("zwei");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,4) == "zehn") {
+                } else if (word.substr(0,4) == "zehn") {
                     decoded_word.push_back("zehn");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,5) == "zwolf"){
+                } else if (word.substr(0,5) == "zwolf") {
                     decoded_word.push_back("zwolf");
                     word.erase(0, 5);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,4) == "zwan"){
+                } else if (word.substr(0,4) == "zwan") {
                     decoded_word.push_back("zwan");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                }
-                else if (word.substr(0,3) == "zig"){
+                } else if (word.substr(0,3) == "zig") {
                     decoded_word.push_back("zig");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                }
-                else{
+                } else {
                     break;
                 }
             }
-
-            }
-
-
-
-
-
+        }
     }
 };
 
+int main(int argc, char * argv []) {
+	if (argc == 2) {
+		auto a = scanner::decode_word(argv[1]);
+		
+		for (int i = 0; i < a.size(); ++i){
+			cout << a[i] << ' ';
+		}
 
+	} else {
+		cout << "Necesitas un argumento" << endl;
+	}
 
-int main() {
-
-
-
-   auto a = scanner::decode_word("zweitausendneunhundertsechsundsiebzig");
-
-   for (int i = 0; i<a.size(); i++){
-       cout << a[i] << " ";
-   }
-
-
-    return 0;
+	return 0;
 }
