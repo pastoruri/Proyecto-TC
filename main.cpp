42312723cf849730315fac8bf79e4bef2cfa1c34
GSR <39766479+fresh-avocado@users.noreply.github.com>
2019-10-31 13:08:24 -0500

    Rule z14 and deciding function missing

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
 
 using namespace std;
 
-struct scanner{
-
-    scanner(){}
-
-    static vector<string> decode_word(string word){
+struct scanner {
+    scanner() = default;
+    static vector<string> decode_word(string word) {
         vector<string> decoded_word;
         decode_aux(word, decoded_word);
         int size_count = 0;
-
-        for(int i=0; i < decoded_word.size(); ++i){
+        for (int i = 0; i < decoded_word.size(); ++i) {
             size_count += decoded_word[i].size();
-		} 
-
-		if (size_count != word.size()) {
-            cout << "There's a typo somewhere in the number passed "<<endl;
         }
-
+        if (size_count != word.size()) {
+            cout << "There's a typo somewhere."<<endl;
+        }
         return decoded_word;
     }
+   static void decode_aux(string word, vector<string>& decoded_word) {
 
-   	static void decode_aux(string word, vector<string> & decoded_word) {
-
-        if(word.size() == 0){
+        if (word.size() == 0) {
             return;
         }
 
@@ -36,17 +31,18 @@ struct scanner{
                     decoded_word.push_back("atch");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else {
                     break;
                 }
             }
-
             case 'd': {
                 if(word.substr(0,4) == "drei") {
                     decoded_word.push_back("drei");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else {
                     break;
                 }
             }
@@ -55,84 +51,95 @@ struct scanner{
                     decoded_word.push_back("ein");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                } else if(word.substr(0,3) == "elf") {
+                } else if (word.substr(0,3) == "elf") {
                     decoded_word.push_back("elf");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                } else{
+                }
+                else {
                     break;
                 }
             }
 
-            case 'f': {
-                if(word.substr(0,4) == "funf") {
+            case 'f' : {
+                if (word.substr(0,4) == "funf") {
                     decoded_word.push_back("funf");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else{
+                }
+                else {
                     break;
                 }
             }
 
-            case 'h': {
-                if(word.substr(0,7) == "hundert") {
-                    decoded_word.push_back("hundert");
-                    word.erase(0, 7);
-                    decode_aux(word, decoded_word);
-                } else {
-                    break;
-      	        }
-            }
+            case 'h' : {
+                    if (word.substr(0,7) == "hundert") {
+                        decoded_word.push_back("hundert");
+                        word.erase(0, 7);
+                        decode_aux(word, decoded_word);
+                    }
+                    else {
+                        break;
+                    }
+                }
 
-            case 'n': {
-                if(word.substr(0,4) == "neun") {
+            case 'n' : {
+                if (word.substr(0,4) == "neun") {
                     decoded_word.push_back("neun");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else{
                     break;
                 }
             }
-            case 's': {
+            case 's' :{
                 if(word[4] == 's' && word.substr(4,8) != "sieb" && word.substr(4,8) != "sech" && word.substr(4,8) != "ssig") {
                     decoded_word.push_back("sechs");
                     word.erase(0, 5);
                     decode_aux(word, decoded_word);
-                } else if (word[4] != 's' && word.substr(0,4) == "sech") {
+                }
+                else if (word[4] != 's' && word.substr(0,4) == "sech") {
                     decoded_word.push_back("sech");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,4) == "sieb" && word.substr(4,6) != "en") {
+                }
+                else if (word.substr(0,4) == "sieb" && word.substr(4,6) != "en"){
                     decoded_word.push_back("sieb");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,6) == "sieben") {
+                }
+                else if (word.substr(0,6) == "sieben"){
                     decoded_word.push_back("sieben");
                     word.erase(0, 6);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,4) == "ssig") {
+                }
+                else if (word.substr(0,4) == "ssig"){
                     decoded_word.push_back("ssig");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else{
                     break;
                 }
             }
-            case 't': {
+            case 't' :{
                 if(word.substr(0,7) == "tausend") {
                     decoded_word.push_back("tausend");
                     word.erase(0, 7);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else{
                     break;
                 }
             }
-            case 'u': {
+            case 'u' :{
                 if(word.substr(0,3) == "und") {
                     decoded_word.push_back("und");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else{
                     break;
                 }
             }
@@ -141,51 +148,393 @@ struct scanner{
                     decoded_word.push_back("vier");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else{
                     break;
                 }
             }
 
             case 'z' :{
-                if( word.substr(0,4) == "zwei" ) {
+                if( word.substr(0,4) =="zwei" ) {
                     decoded_word.push_back("zwei");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,4) == "zehn") {
+                }
+                else if (word.substr(0,4) == "zehn") {
                     decoded_word.push_back("zehn");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,5) == "zwolf") {
+                }
+                else if (word.substr(0,5) == "zwolf"){
                     decoded_word.push_back("zwolf");
                     word.erase(0, 5);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,4) == "zwan") {
+                }
+                else if (word.substr(0,4) == "zwan"){
                     decoded_word.push_back("zwan");
                     word.erase(0, 4);
                     decode_aux(word, decoded_word);
-                } else if (word.substr(0,3) == "zig") {
+                }
+                else if (word.substr(0,3) == "zig"){
                     decoded_word.push_back("zig");
                     word.erase(0, 3);
                     decode_aux(word, decoded_word);
-                } else {
+                }
+                else{
                     break;
                 }
             }
-        }
+
+            }
+
+
+
+
+
     }
 };
 
-int main(int argc, char * argv []) {
-	if (argc == 2) {
-		auto a = scanner::decode_word(argv[1]);
-		
-		for (int i = 0; i < a.size(); ++i){
-			cout << a[i] << ' ';
-		}
+vector<string> z1 = {"ein", "zwei", "drei", "vier", "funf", "sechs", "sieben", "acht", "neun"}; 
+vector<string> z2 = {"zehn", "elf", "zwolf"};
+vector<string> z3 = {"drei", "vier", "funf", "sech", "sieb", "acht", "neun" };
+vector<string> z4 = {"zwan", "vier", "funf", "sech", "sieb", "acht", "neun" };
+vector<string> z5 = {"hundert"};
+vector<string> z6 = {"tausend"};
 
+bool is_in_z2(const string& w) {
+	bool matched = false;
+	for (auto& s : z2)
+		if (s == w)
+			matched = true;
+	return matched;
+}
+
+bool rule1(const vector<string>& svec, unsigned index) {
+	string s = svec[index];
+
+	bool accepts = false;
+
+	for (auto& word : z1)
+		if (word == s)
+			accepts = true;
+
+	return accepts;
+}
+
+bool rule2(const vector<string>& svec, unsigned index) {
+	string s = svec[index];
+	string next_word = svec[index+1];
+	bool word_matched = false;
+
+	for (auto& word : z3)
+		if (word == s)
+			word_matched = true;
+
+	if (word_matched && next_word == "zehn")
+		return true;
+	else
+		return false;
+}
+
+bool rule71(const vector<string>& svec, unsigned index) {
+	string s = svec[index];
+	string next_word = svec[index+1];
+	bool word_matched = false;
+
+	for (auto& word : z4)
+		if (word == s)
+			word_matched = true;
+
+	if (word_matched && next_word == "zig")
+		return true;
+	else
+		return false;
+	
+}
+
+bool rule72(const vector<string>& svec, unsigned index) {
+	string s = svec[index];
+	string next_word = svec[index+1];
+
+	return (s == "drei" && next_word == "ssig");
+}
+
+bool rule7(const vector<string>& svec, unsigned index) {
+	string curr_word = svec[index];
+	string next_word = svec[index+1];
+
+	if (curr_word == "drei")
+		return rule72(svec, index);
+	else
+		return rule71(svec, index);
+}
+
+bool ruleU(const vector<string>& svec, unsigned index) {
+	string s = svec[index];
+	string next_word = svec[index+1];
+	bool word_matched = false;
+
+	word_matched = rule1(svec, index);
+
+	if (word_matched && next_word == "und")
+		return true;
+	else
+		return false;
+}
+
+bool rule9(const vector<string>& svec, unsigned index) {	
+	string s = svec[index];
+	string next_word = svec[index+1];
+	bool word_matched1 = false;
+	bool word_matched2 = false;
+
+	word_matched1 = rule1(svec, index);
+
+	for (auto& word : z5)
+		if (word == next_word)
+			word_matched2 = true;
+
+	if (word_matched1 && word_matched2)
+		return true;
+	else
+		return false;
+}
+
+bool rule8(const vector<string>& svec, unsigned index) {
+	bool ruleU_accepted = ruleU(svec, index);
+	bool rule7_accepted = false;
+
+	rule7_accepted = rule7(svec, index+2);
+
+	if (ruleU_accepted && rule7_accepted)
+		return true;
+	else 
+		return false;
+}
+
+pair<bool, unsigned> rule10(const vector<string>& svec, unsigned index) {
+	unsigned offset = 0;
+	string s = svec[index];
+	bool should_use_rule2 = true;
+	for (auto& word : z2)
+		if (word == s)
+			should_use_rule2 = false;
+
+	bool rule2_accepted = false;
+	bool next_word_in_z5 = false;
+
+	if (should_use_rule2) {
+		rule2_accepted = rule2(svec, index);
+		if (svec[index+2] == "hundert") {
+			next_word_in_z5 = true;
+			offset = 3;
+		}
 	} else {
-		cout << "Necesitas un argumento" << endl;
+		rule2_accepted = true;
+		if (svec[index+1] == "hundert") {
+			next_word_in_z5 = true;
+			offset = 2;
+		}
 	}
+
+	return make_pair(rule2_accepted && next_word_in_z5, offset);
+}
+
+pair<bool, unsigned> rule11(const vector<string>& svec, unsigned index) {
+	unsigned offset = 0;
+	string s = svec[index];
+	bool rule5_accepts = false;
+	bool rule9_accepts = false;
+	if (s == "hundert") {
+		rule5_accepts = true;
+		++offset;
+	} else {
+		rule9_accepts = rule9(svec, index);
+		if (rule9_accepts)
+			offset += 2;
+	}
+
+	bool rule5_or_rule9_accepts = rule5_accepts || rule9_accepts;
+
+	if (rule5_or_rule9_accepts == false) {
+		return make_pair(false, offset);
+	}
+
+	// CHECK FOR RULE 1, 2, 7, 8 ACCEPTANCE
+	
+	// checking rule 1
+	bool rule1_accepts = false;
+	if (rule9_accepts) {
+		rule1_accepts = rule1(svec, index+2);
+	} else {
+		rule1_accepts = rule1(svec, index+1);
+	}
+
+	if (rule1_accepts && rule5_or_rule9_accepts) {
+		++offset;
+		return make_pair(true, offset);
+	}
+
+	// at this point, rule5 or rule 9 accepts, but rule1 doesn't
+	// checking rule 2
+	bool rule2_accepts = false;
+	if (rule9_accepts) {
+		if (is_in_z2(svec[index+2])) {
+			rule2_accepts = true;
+			++offset;
+		} else {
+			rule2_accepts = rule2(svec, index+2);
+			if (rule2_accepts)
+				offset += 2;
+		}
+	} else {
+		if (is_in_z2(svec[index+1])) {
+            rule2_accepts = true;
+			++offset;
+        } else {
+            rule2_accepts = rule2(svec, index+1);
+			if (rule2_accepts)
+				offset += 2;
+        }
+	}
+	
+	if (rule2_accepts && rule5_or_rule9_accepts)
+		return make_pair(true, offset);
+
+	// at this point, rule5 or rule9 accepts, but rule2 doesn't
+	// checking rule 8
+
+	bool rule8_accepts = false;
+	if (rule9_accepts) {
+		rule8_accepts = rule8(svec, index+2);
+	} else {
+		rule8_accepts = rule8(svec, index+1);
+	}
+
+	if (rule8_accepts && rule5_or_rule9_accepts) {
+		offset += 4;
+		return make_pair(true, offset);
+	}
+
+	// at this point, rule5 or rule9 accepts, but rule8 doesn't
+	// checking rule 7
+	
+	bool rule7_accepts = false;
+	if (rule9_accepts) {
+		rule7_accepts = rule7(svec, index+2);	
+	} else {
+		rule7_accepts = rule7(svec, index+1);
+	}
+	
+	if (rule7_accepts && rule5_or_rule9_accepts) {
+		offset += 2;
+		return make_pair(true, offset);
+	} else {
+		return make_pair(false, offset);
+	}
+}
+
+bool rule12(const vector<string>& svec, unsigned index) {
+	pair<bool, unsigned> rule10_a = rule10(svec, index);
+	bool rule10_accepts = rule10_a.first;
+	if (!rule10_accepts)
+		return false;
+
+	unsigned offset = rule10_a.second;
+
+	// checking rule 1
+	bool rule1_accepts = rule1(svec, index+offset);
+	if (rule10_accepts && rule1_accepts)
+		return true;
+
+	// checking rule 2
+	bool rule2_accepts = false;
+	if (is_in_z2(svec[index+offset])) {
+		rule2_accepts = true;		
+	} else {
+		rule2_accepts = rule2(svec, index+offset);
+	}
+	if (rule10_accepts && rule2_accepts)
+		return true;
+
+	// checking rule 8
+	bool rule8_accepts = rule8(svec, index+offset);
+	if (rule10_accepts && rule8_accepts)
+		return true;
+
+	// checking rule 7
+	bool rule7_accepts = rule7(svec, index+offset);
+	if (rule10_accepts && rule7_accepts)
+		return true;
+	else
+		return false;
+}
+
+pair<bool, unsigned> rule13(const vector<string>& svec, unsigned index) {
+	
+	// checking rule 11 and 6
+	bool rule11_a = rule11(svec, index);
+	bool rule11_accepts = rule11_a.first;
+	unsigned offset = rule11_a.second;
+	bool rule6_accepts = (svec[index+offset] == "tausend");
+	if (rule11_accepts && rule6_accepts)
+		return true;
+	
+	// checking rule 9 and 6
+	bool rule9_accepts = rule9(svec, index);
+	rule6_accepts = (svec[index+2] == "tausend");
+	if (rule9_accepts && rule6_accepts)
+		return true;
+
+	// checking rule 7 and 6
+	bool rule7_accepts = rule7(svec, index);
+	rule6_accepts = (svec[index+2] == "tausend");
+	if (rule7_accepts && rule6_accepts)
+		return true;
+
+	// checking rule 1 and 6
+	bool rule1_accepts = rule1(svec, index);
+	rule6_accepts = (svec[index+1] == "tausend");
+	if (rule1_accepts && rule6_accepts)
+		return true;
+
+	// checking rule 2 and 6
+	bool rule2_accepts = false;
+	if (is_in_z2(svec[index])) {
+		rule2_accepts = true;
+		rule6_accepts = (svec[index+1] == "tausend");
+	} else {
+		rule2_accepts = rule2(svec, index);
+		rule6_accepts = (svec[index+2] == "tausend");
+	}
+	if (rule2_accepts && rule6_accepts)
+		return true;
+
+	// checking rule 8
+	bool rule8_accepts = rule8(svec, index);
+	rule6_accepts = (svec[index+3] == "tausend");
+	if (rule8_accepts && rule6_accepts)
+		return true;
+	else
+		return false;
+}
+
+int main(int argc, char** argv) {
+	// TODO: function that chooses a starting rule
+	// TODO: regla 14
+	// TODO: fix FIXME
+	string word;
+
+	cout << "Enter a number in German: ";
+	cin >> word;
+
+	vector<string> word_parts = scanner::decode_word(word);
+
+	for (int i = 0; i < word_parts.size(); i++)
+	   cout << word_parts[i] << " ";
+	cout << endl;
+
+	cout << "Word accepted? " << rule12(word_parts, 0) << endl;
 
 	return 0;
 }
